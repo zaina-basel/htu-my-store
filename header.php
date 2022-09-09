c1b2dd64a5dca9a07b458530b6d971c699953128
zaina basel <107751136+zaina-basel@users.noreply.github.com>
2022-09-09 11:16:50 +0300

    Add files via upload

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title><?php echo isset($this->title) ? $this->title : 'MyStore';?></title>
+        <title><?php echo isset($this->title) ? $this->title : 'HTU Store';?></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/png" href="<?php echo APP_URL;?>public/images/logo24.png">
@@ -11,7 +11,12 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" type="text/css">
         <!-- Custom JS -->
         <link href="<?php echo APP_URL;?>public/css/sidebar-menu.css" rel="stylesheet">
-        <link href="<?php echo APP_URL;?>public/css/login.css" rel="stylesheet"> 
+        <link href="<?php echo APP_URL;?>public/css/mystore.css" rel="stylesheet"> 
     </head>
     <body>
-
+        <div id="main">
+        <?php 
+            require 'navbar.php';
+        ?>
+        <div id="wrapper" class="">
+        
--- index.php
+++ index.php
@@ -1,36 +1,53 @@
-<?php
+<div id="page-content-wrapper">
+    <div id="page-content" style="margin-bottom: 50px;">
+        <h3 class="page-title" id="business-info"><u>SubCategory:</u>
+            <span class="right-menu"><a href="<?php echo APP_URL; ?>subcategory/create"><i class="fa fa-plus-circle"></i> New SubCategory</a></span>
+        </h3>
+        <div class="row">
+            <div class="col-xs-12 table-responsive" style="">
 
-class Index extends Controller {
+                <?php
+                $listItem = '';
+                $nbrItem = 0;
+                foreach ($this->subcategory as $key => $value) {
+                    $nbrItem++;
+                    $sub_category_id = $value['sub_category_id'];
 
-    function __construct() {
-        parent::__construct();
-        
-        if(!isset($_SESSION['logged_in'])){
-            header("location: " . APP_URL . "login");
-        }
-    }
+                    $listItem .= '
+                        <tr id="tr_' . $sub_category_id . '">
+                            <td>' . $sub_category_id . '</td>
+                            <td>' . $value['sub_category_name'] . '</td>
+                            <td>' . $value['category_name'] . '</td>
+                            <td><a href="' . APP_URL . 'subcategory/edit/' . $sub_category_id . '" title="Edit" class="btn btn-sm btn-success edit"><i class="fa fa-pencil"></i></a></td>
+                            <td><a href="#" rel="' . $sub_category_id . '" title="Delete" class="btn btn-sm btn-danger delete_subcategory"><i class="fa fa-times"></i></a></td>
+                        </tr>';
+                }
+                ?>
+                <?php if ($nbrItem > 0) { ?>
+                    <table id="datatable" class="table table-striped- table-bordered table-hover" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Id#</th>
+                                <th>SubCategory Name</th>
+                                <th>Category</th>
+                                <th style="width: 60px">Edit</th>
+                                <th style="width: 60px">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php echo $listItem; ?>    
+                        </tbody> 
+                    </table>
+                <?php } else { ?>
+                    <div class="alert alert-warning alert-dismissable">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                        <h4>No SubCategory exist yet!</h4>
+                    </div>
+                <?php } ?>
 
-    /*
-     * Default Home page
-     */
-    public function index() {
-        $this->view->title = 'MyStore | Home';
-        
-        /*
-         * 
-         */
-        $this->view->number_category = $this->model->number_category();
-        $this->view->number_subcategory = $this->model->number_subcategory();
-        $this->view->number_product = $this->model->number_product();
-        
-        
-        /*
-         * Load view
-         */
-        $this->view->render('includes/header');
-        $this->view->render('includes/sidebar');
-        $this->view->render('home/index');
-        $this->view->render('includes/footer');
-    }
 
-}
+
+            </div>
+        </div>  
+    </div>
+</div>
